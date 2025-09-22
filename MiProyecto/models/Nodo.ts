b155748a2fd48c741f7068966c586f0a359d499b
AbrahamRP97 <abrahamj.rivera97@gmail.com>
2025-09-21 21:38:23 -0600

    probando commitear desde repositorio local

--- MiProyecto/models/Nodo.ts
+++ MiProyecto/models/Nodo.ts
@@ -5,7 +5,7 @@ export class Nodo {
   urgencia: 1 | 2 | 3;
   expediente: string;
   next: Nodo | null;
-
+//PROBANDO SI SE GUARDA EN EL REPOSITORIO
   constructor(nombre: string, edad: string, sintomas: string, urgencia: 1 | 2 | 3, expediente: string) {
     this.nombre = nombre;
     this.edad = edad;
